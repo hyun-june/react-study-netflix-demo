9270f101887c75184a8cdac5d304aaf3f0ccc241
unknown <leehyunjune11@naver.com>
2025-11-07 14:07:07 +0900

    fix

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import "./MoviePage.style.css"
-import { useSearchMovieQuery } from '../../hooks/useSearchMovie'
-import { useSearchParams } from 'react-router-dom';
-import { Alert, Col, Container, Row, Spinner } from 'react-bootstrap';
-import MovieCard from '../../common/MovieCard/MovieCard';
-import ReactPaginate from 'react-paginate';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { useMoviesIdList } from '../../hooks/useMoviesIdList';
+import React, { useState } from "react";
+import "./MoviePage.style.css";
+import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
+import { useSearchParams } from "react-router-dom";
+import { Alert, Col, Container, Row, Spinner } from "react-bootstrap";
+import MovieCard from "../../common/MovieCard/MovieCard";
+import ReactPaginate from "react-paginate";
+import Dropdown from "react-bootstrap/Dropdown";
+import { useMoviesIdList } from "../../hooks/useMoviesIdList";
 
 // 경로 2가지
 // nav바에서 클릭해서 온경우 => nowplayingMovie 보여주기
@@ -17,117 +17,144 @@ import { useMoviesIdList } from '../../hooks/useMoviesIdList';
 // 페이지네이션 클릭할때마다 page 바꿔주기
 // page 값이 바뀔때 마다 useSearchMovie에 page까지 넣어서 fetch
 const MoviePage = () => {
-  const [query,setQuery] = useSearchParams();
+  const [query, _setQuery] = useSearchParams();
   const keyword = query.get("q");
-  const [page,setPage] =useState(1);
-  const [selectFilter,setSelectFilter] = useState('default')
+  const [page, setPage] = useState(1);
+  const [selectFilter, setSelectFilter] = useState("default");
   const [selectedValue, setSelectedValue] = useState(null);
 
-  const {data,isLoading,isError,error} = useSearchMovieQuery({keyword,page});
-  console.log("111",data)
-  const {data:idData,isLoading:idisLoading,isError:idisError,error:iderror} = useMoviesIdList()
-  console.log(idData)
-  const handlePageClick =({selected})=>{
-    setPage(selected+1)
-  }
+  const { data, isLoading, isError, error } = useSearchMovieQuery({
+    keyword,
+    page,
+  });
+  const {
+    data: idData,
+    isLoading: idisLoading,
+    isError: idisError,
+    error: _iderror,
+  } = useMoviesIdList();
+  const handlePageClick = ({ selected }) => {
+    setPage(selected + 1);
+  };
 
-  const dataSort = () =>{
+  const dataSort = () => {
     if (!data || !data.results) return [];
-    let filterData = [...data.results]
+    let filterData = [...data.results];
 
-    if(selectFilter === 'popularity'){
-      return filterData.sort((a,b)=>b.popularity - a.popularity);
+    if (selectFilter === "popularity") {
+      return filterData.sort((a, b) => b.popularity - a.popularity);
     }
-    if(selectFilter === 'average'){
-      return filterData.sort((a,b)=> b.vote_average - a.vote_average);
-    }
-    
-    if(selectedValue) {
-      filterData = filterData.filter((movie)=>movie.genre_ids.includes(selectedValue))
+    if (selectFilter === "average") {
+      return filterData.sort((a, b) => b.vote_average - a.vote_average);
     }
 
-    return filterData
-  }
+    if (selectedValue) {
+      filterData = filterData.filter((movie) =>
+        movie.genre_ids.includes(selectedValue)
+      );
+    }
+
+    return filterData;
+  };
 
   if (isLoading && idisLoading) {
-    return <div>
-      <Spinner animation='border' variant='danger' style={{width:"5rem",height:"5rem"}} />
-    </div>;
-    
+    return (
+      <div>
+        <Spinner
+          animation="border"
+          variant="danger"
+          style={{ width: "5rem", height: "5rem" }}
+        />
+      </div>
+    );
   }
   if (isError && idisError) {
     return <Alert variant="danger">{error.message}</Alert>;
   }
 
-  const selectGenre = (id) =>{
-    setSelectedValue(id)
-  }
+  const selectGenre = (id) => {
+    setSelectedValue(id);
+  };
 
   return (
     <Container>
       <Row>
-
         <Col lg={3} xs={12}>
-        <Dropdown>
-        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-              {selectFilter === 'default' ? '기본' : selectFilter === 'popularity' ? '인기순' : '평점순'}
-        </Dropdown.Toggle>
+          <Dropdown>
+            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+              {selectFilter === "default"
+                ? "기본"
+                : selectFilter === "popularity"
+                ? "인기순"
+                : "평점순"}
+            </Dropdown.Toggle>
 
-        <Dropdown.Menu>
-
-        {selectFilter !== 'default' && (
-                <Dropdown.Item onClick={() => setSelectFilter('default')}>
+            <Dropdown.Menu>
+              {selectFilter !== "default" && (
+                <Dropdown.Item onClick={() => setSelectFilter("default")}>
                   기본
                 </Dropdown.Item>
               )}
-              {selectFilter !== 'popularity' && (
-                <Dropdown.Item onClick={() => setSelectFilter('popularity')}>
+              {selectFilter !== "popularity" && (
+                <Dropdown.Item onClick={() => setSelectFilter("popularity")}>
                   인기순
                 </Dropdown.Item>
               )}
-              {selectFilter !== 'average' && (
-                <Dropdown.Item onClick={() => setSelectFilter('average')}>
+              {selectFilter !== "average" && (
+                <Dropdown.Item onClick={() => setSelectFilter("average")}>
                   평점순
                 </Dropdown.Item>
               )}
-        </Dropdown.Menu>
-      </Dropdown>
-        <div className='badge-area'>{idData.map((item)=>(<div className='genre-badge-btn' onClick={()=>selectGenre(item.id)} >{item.name}</div>))}</div>
+            </Dropdown.Menu>
+          </Dropdown>
+          <div className="badge-area">
+            {idData.map((item) => (
+              <div
+                className="genre-badge-btn"
+                onClick={() => selectGenre(item.id)}
+              >
+                {item.name}
+              </div>
+            ))}
+          </div>
         </Col>
         <Col lg={9} xs={12}>
           <Row>
-          {dataSort().map((movie,index)=><Col className='movie-section' lg={3} xs={12} key={index}><MovieCard movie={movie} /></Col>)}
+            {dataSort().map((movie, index) => (
+              <Col className="movie-section" lg={3} xs={12} key={index}>
+                <MovieCard movie={movie} />
+              </Col>
+            ))}
           </Row>
         </Col>
-        <div className='pagination-area'>
+        <div className="pagination-area">
           {data?.total_pages > 1 && (
-              <ReactPaginate
-                nextLabel="next >"
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={3}
-                marginPagesDisplayed={2}
-                pageCount={data?.total_pages}
-                forcePage={page - 1}
-                previousLabel="< previous"
-                pageClassName="page-item"
-                pageLinkClassName="page-link"
-                previousClassName="page-item"
-                previousLinkClassName="page-link"
-                nextClassName="page-item"
-                nextLinkClassName="page-link"
-                breakLabel="..."
-                breakClassName="page-item"
-                breakLinkClassName="page-link"
-                containerClassName="pagination"
-                activeClassName="active"
-                renderOnZeroPageCount={null}
-
-              />
-            )}
+            <ReactPaginate
+              nextLabel="next >"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={3}
+              marginPagesDisplayed={2}
+              pageCount={data?.total_pages}
+              forcePage={page - 1}
+              previousLabel="< previous"
+              pageClassName="page-item"
+              pageLinkClassName="page-link"
+              previousClassName="page-item"
+              previousLinkClassName="page-link"
+              nextClassName="page-item"
+              nextLinkClassName="page-link"
+              breakLabel="..."
+              breakClassName="page-item"
+              breakLinkClassName="page-link"
+              containerClassName="pagination"
+              activeClassName="active"
+              renderOnZeroPageCount={null}
+            />
+          )}
         </div>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default MoviePage
+export default MoviePage;
